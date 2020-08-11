2666d30cf4f0a57f673e1e251f5f0f924a70019b
parkyes90 <parkyes90@gmail.com>
2020-08-11 11:52:18 +0900

    add typing

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((response) => {
-  console.log(response.data);
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response: AxiosResponse<Todo>) => {
+  const { id, title, completed } = response.data;
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
 });
